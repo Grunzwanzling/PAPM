b8c66bf5dcc0a61f09358c147b223a651b4964ce
Maximilian von Gaisberg <github@vongaisberg.de>
2018-12-24 13:22:00 +0100

    Added terminal flags to unlock TUI

--- unlock.go
+++ unlock.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
+	"os"
+	"strings"
 )
+
+var socket string
+var database_list string
 
 func readLines(path string) ([]string, error) {
 	file, err := os.Open(path)
@@ -19,16 +26,38 @@ func readLines(path string) ([]string, error) {
 	}
 	return lines, scanner.Err()
 }
+func readFlags() {
+
+	flag.StringVar(&socket, "socket", "./socket", "a filepath")
+	flag.StringVar(&database_list, "db_list", "./db_list", "a filepath")
+	flag.Parse()
+
+	wd, _ := os.Getwd()
+	socket = strings.Replace(socket, ".", wd, -1)
+	database_list = strings.Replace(database_list, ".", wd, -1)
+
+}
 func main() {
+	readFlags()
+	dbs, err := readLines(database_list)
+	if err != nil {
+		println("Read error: ", err.Error())
+		os.Exit(0)
+	}
+
 	app := tview.NewApplication()
 	form := tview.NewForm().SetLabelColor(tcell.ColorWhite).
-		AddDropDown("Database", []string{"Mr.", "Ms.", "Mrs.", "Dr.", "Prof."}, 0, nil).
+		AddDropDown("Database", dbs, 0, nil).
 		AddPasswordField("Password", "", 50, '*', nil).
 		AddButton("Unlock", nil).
 		AddButton("Quit", func() {
 			app.Stop()
+		}).
+		SetCancelFunc(func() {
+			app.Stop()
 		})
-	form.SetBorder(true).SetTitle("Enter some data").SetTitleAlign(tview.AlignLeft)
+
+	form.SetBorder(true).SetTitle("Unlock a database").SetTitleAlign(tview.AlignLeft)
 	if err := app.SetRoot(form, true).SetFocus(form).Run(); err != nil {
 		panic(err)
 	}
