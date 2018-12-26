43436bdfc53f995399c1cc5a70cc10a3daf4b662
Maximilian von Gaisberg <github@vongaisberg.de>
2018-12-26 13:01:32 +0100

    Addde the comminucation to the server in unlock.go

--- unlock.go
+++ unlock.go
@@ -3,14 +3,22 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
+	"io"
+	"net"
 	"os"
 	"strings"
+	"time"
 )
 
 var socket string
 var database_list string
+var form *tview.Form
+var pwField *tview.InputField
+var dropdownField *tview.DropDown
+var app *tview.Application
 
 func readLines(path string) ([]string, error) {
 	file, err := os.Open(path)
@@ -37,6 +45,37 @@ func readFlags() {
 	database_list = strings.Replace(database_list, ".", wd, -1)
 
 }
+func readOnce(r io.Reader) {
+	buf := make([]byte, 1024)
+	n, err := r.Read(buf[:])
+	if err != nil {
+		println("Read error: ", err.Error())
+		os.Exit(1)
+	}
+	app.Stop()
+	fmt.Println(string(buf[0:n]))
+
+}
+func sendCommand() {
+
+	c, err := net.Dial("unix", socket)
+	if err != nil {
+		println("Dial error: ", err.Error())
+		os.Exit(1)
+	}
+	defer c.Close()
+
+	go readOnce(c)
+	_, db := dropdownField.GetCurrentOption()
+	pw := pwField.GetText()
+	msg := "unlock;" + db + ";" + pw
+	_, err = c.Write([]byte(msg))
+	if err != nil {
+		println("Write error: ", err)
+	}
+	time.Sleep(1e9)
+	app.Stop()
+}
 func main() {
 	readFlags()
 	dbs, err := readLines(database_list)
@@ -44,12 +83,19 @@ func main() {
 		println("Read error: ", err.Error())
 		os.Exit(0)
 	}
+	pwField = tview.NewInputField().
+		SetLabel("Password").
+		SetMaskCharacter('*')
 
-	app := tview.NewApplication()
-	form := tview.NewForm().SetLabelColor(tcell.ColorWhite).
-		AddDropDown("Database", dbs, 0, nil).
-		AddPasswordField("Password", "", 50, '*', nil).
-		AddButton("Unlock", nil).
+	dropdownField = tview.NewDropDown().
+		SetLabel("Database").
+		SetOptions(dbs, nil).
+		SetCurrentOption(0)
+	app = tview.NewApplication()
+	form = tview.NewForm().SetLabelColor(tcell.ColorWhite).
+		AddFormItem(dropdownField).
+		AddFormItem(pwField).
+		AddButton("Unlock", sendCommand).
 		AddButton("Quit", func() {
 			app.Stop()
 		}).
