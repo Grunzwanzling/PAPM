26f7311c989ab020098994dd5a7bb0e5b58954d5
Maximilian von Gaisberg <github@vongaisberg.de>
2018-12-30 23:08:58 +0100

    Unlocking does now also connect to the wifi

--- unlock.sh
+++ unlock.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-unset password
-prompt="Enter Password:"
-while IFS= read -p "$prompt" -r -s -n 1 char 
-do
-    if [[ $char == $'\0' ]];     then
-        break
-    fi
-    if [[ $char == $'\177' ]];  then
-        prompt=$'\b \b'
-        password="${password%?}"
-    else
-        prompt='*'
-        password+="$char"
-    fi
-done
-echo " "
-echo "Done. Password=$password" 
+/home/max/git/YouShallNotPassword/unlock -socket /home/max/git/YouShallNotPassword/socket -db /home/max/pass/test.kdbx
+/home/max/git/YouShallNotPassword/wifi_pass.sh
